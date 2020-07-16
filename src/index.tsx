1bd00078586be6f0ea0572f2e08e034021e4a637
Ernest Oliver Casanes <ecasanes@gmail.com>
2020-07-16 23:38:37 +0800

    redux-persistent with devtools

--- src/index.tsx
+++ src/index.tsx
@@ -6,13 +6,22 @@ import * as serviceWorker from './serviceWorker';
 
 import { Provider } from 'react-redux';
 import { configureStore } from './store';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/lib/integration/react';
+
+const store = configureStore();
+const persistor = persistStore(store);
 
 ReactDOM.render(
-  <Provider store={configureStore()}>
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-  </Provider>,
+    <Provider store={store}>
+      <PersistGate 
+      loading={<div>Loding...</div>}
+      persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
--- src/store.ts
+++ src/store.ts
@@ -1,13 +1,31 @@
 import { createStore, combineReducers } from 'redux';
 import { todos } from './components/reducers';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 
 const reducers = {
     todos
 };
 
-const rootReducer = combineReducers(reducers);
+const persistConfig = {
+    key: 'root',
+    storage,
+    stateReconciler: autoMergeLevel2,
+}
 
-export const configureStore = () => createStore(rootReducer);
+const rootReducer:any = combineReducers(reducers);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// export const configureStore = () => createStore(persistedReducer);
+let theWindow:any = window;
+const reduxDevTools:any = theWindow.__REDUX_DEVTOOLS_EXTENSION__;
+
+// to include redux dev tools
+export const configureStore = () => createStore(
+    persistedReducer,
+    reduxDevTools && reduxDevTools()
+);
 
 // STORE -> Globalized State
 // ACTION -> Describes What do you want to do
