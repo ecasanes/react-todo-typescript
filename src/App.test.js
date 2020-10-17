89ec012304a13b333328e905b52f1a21dd99ddb3
Ernest Oliver Casanes <ecasanes@gmail.com>
2020-10-18 01:27:41 +0800

    init misc and test

---
+++ src/App.test.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-lone-blocks */
+// import React from 'react';
+// import {
+//   render
+// } from '@testing-library/react';
+// import App from './App';
+
+test('renders learn react link', () => {
+  // const {
+  //   getByText
+  // } = render( < App / > );
+  {
+    /* const linkElement = getByText(/learn react/i);
+      expect(linkElement).toBeInTheDocument(); */
+  }
+  expect(true);
+});
